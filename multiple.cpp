1ffbacabc2d0b680964763516b1c689d67c11564
7022667205 <yashu7022667205@gmail.com>
2025-05-22 16:04:48 +0530

    Add files via upload

---
+++ multiple.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+class circle{
+private:
+   float radius;
+public:
+   void setradius(float){
+    radius= r;
+   }
+   void area() {
+    cout <, "Area :"<< 3.14 " radius " radius << endl;
+   }
+};
+int main() {
+    circle c;
+    c.setradius(5);
+    c.area();
+    return 0;
+}
